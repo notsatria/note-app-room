3ee31ffddea062aaa932dfb90d0abcd5140f5747
notsatria <buanasatriaa@gmail.com>
2024-03-21 21:28:30 +0700

    feat: Adding layouts

---
+++ app/src/main/java/com/notsatria/noteapp/AddNoteFragment.kt
@@ -0,0 +1,20 @@
+package com.notsatria.noteapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+class AddNoteFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_note, container, false)
+    }
+}
