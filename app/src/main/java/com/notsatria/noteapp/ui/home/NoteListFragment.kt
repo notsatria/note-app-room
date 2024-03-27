5ecc29ad76eb89b7097c49bac1e983b71f8f316c
notsatria <buanasatriaa@gmail.com>
2024-03-27 14:46:10 +0700

    feat: update starter code

--- app/src/main/java/com/notsatria/noteapp/ui/home/NoteListFragment.kt
+++ app/src/main/java/com/notsatria/noteapp/ui/home/NoteListFragment.kt
@@ -1,6 +1,5 @@
 package com.notsatria.noteapp.ui.home
 
-import android.app.Application
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
@@ -10,7 +9,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.notsatria.noteapp.R
@@ -28,29 +26,16 @@ class NoteListFragment : Fragment() {
     ): View {
         _binding = FragmentNoteListBinding.inflate(inflater, container, false)
 
-//        val viewModel = obtainViewModel(requireActivity().application)
+        return binding.root
+    }
 
-//        viewModel.getAllNotes().observe(viewLifecycleOwner) { notes ->
-//            Log.d("NoteListFragment", "onCreateView: $notes")
-//            if (notes != null) {
-//                val adapter = NotesAdapter(notes)
-//                showRecyclerList(adapter)
-//
-//                if (adapter.itemCount == 0) {
-//                    binding.rvNotes.visibility = View.GONE
-//                    binding.emptyView.visibility = View.VISIBLE
-//                } else {
-//                    binding.rvNotes.visibility = View.VISIBLE
-//                    binding.emptyView.visibility = View.GONE
-//                }
-//            }
-//        }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
         binding.fabAddNote.setOnClickListener { view ->
+            Log.d(TAG, "onCreateView: fabAddNote clicked")
             view.findNavController().navigate(R.id.action_noteListFragment_to_addNoteFragment)
         }
-
-        return binding.root
     }
 
     override fun onDestroy() {
@@ -62,55 +47,33 @@ class NoteListFragment : Fragment() {
         binding.rvNotes.layoutManager =
             StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
         binding.rvNotes.adapter = adapter
-//
-//        adapter.setOnItemClickCallback(object : NotesAdapter.OnItemClickCallback {
-//            override fun onItemClicked(view: View, note: NoteEntity) {
-//                Log.d("NoteListFragment", "onItemClicked: $note")
-//                val mBundle = Bundle()
-//                mBundle.putParcelable(EXTRA_NOTE, note)
-//                view.findNavController().navigate(
-//                    R.id.action_noteListFragment_to_addNoteFragment,
-//                    mBundle
-//                )
-//            }
-//
-//            override fun onItemLongClicked(note: NoteEntity) {
-//                Log.d("NoteListFragment", "onItemLongClicked: $note")
-//                showDeleteDialog(requireContext(), note)
-//            }
-//        }
-//        )
     }
-//    private fun obtainViewModel(application: Application): HomeViewModel {
-//        val factory = ViewModelFactory.getInstance(application)
-//        return ViewModelProvider(requireActivity(), factory).get(HomeViewModel::class.java)
-//    }
 
-//    private fun showDeleteDialog(context: Context) {
-//        val builder = AlertDialog.Builder(context)
-//        builder
-//            .setTitle("Delete Note")
-//            .setMessage("Are you sure you want to delete this note?")
-//            .setPositiveButton("Yes") { dialog, _ ->
-//                val viewModel = obtainViewModel(requireActivity().application)
-//                viewModel.delete(note)
-//                dialog.dismiss()
-//                showToast("Note deleted")
-//            }
-//            .setNegativeButton("No") { dialog, _ ->
-//                dialog.dismiss()
-//            }
-//
-//        val dialog = builder.create()
-//
-//        dialog.show()
-//    }
+
+    private fun showDeleteDialog(context: Context) {
+        val builder = AlertDialog.Builder(context)
+        builder
+            .setTitle("Delete Note")
+            .setMessage("Are you sure you want to delete this note?")
+            .setPositiveButton("Yes") { dialog, _ ->
+                dialog.dismiss()
+                showToast("Note deleted")
+            }
+            .setNegativeButton("No") { dialog, _ ->
+                dialog.dismiss()
+            }
+
+        val dialog = builder.create()
+
+        dialog.show()
+    }
 
     private fun showToast(message: String) {
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
     }
 
     companion object {
+        val TAG = NoteListFragment::class.java.name
         val EXTRA_NOTE = "extra_note"
     }
 }
